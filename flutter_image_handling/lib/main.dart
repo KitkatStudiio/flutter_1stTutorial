60031941af9159c131bb4adc1c0abede3d7115aa
Pranav Kumar Verma <39586967+pranavtechnossus@users.noreply.github.com>
2019-05-18 18:26:29 +0530

    StateLess Widget
    Added stateless widget in app.dart and also removed widget from main to app class

--- flutter_image_handling/lib/main.dart
+++ flutter_image_handling/lib/main.dart
@@ -1,26 +1,11 @@
 // I need to import helperlibraries form flutter to get content on the screen
 import 'package:flutter/material.dart';
+import 'src/app.dart';
  
 //Define a 'main' function to run when our app starts
 void main() {
 
-// Create a new tex widget to show some text on the screen
-var app = MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(title: Text('Lets see some Images')
-    ),
-    floatingActionButton: FloatingActionButton(
-      child: Icon(
-        Icons.add
-      ),
-      onPressed: () {
-        print('hi There');
-      },
-    ),
-  ),
-);
-
 // Take that widget and get it on the screen
-runApp(app);
+runApp(App());
 
 }
--- flutter_image_handling/lib/src/app.dart
+++ flutter_image_handling/lib/src/app.dart
@@ -2,4 +2,25 @@ import 'package:flutter/material.dart';
 
 
 // Create a class that will be our custom widget
- 
+ // This class must extend the 'StateLess Widget' base class
+ class App extends StatelessWidget {
+
+   //Must define a 'build' method that returns the widgets that *this* widget will show
+   Widget build(context) {
+   return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Lets see some Images')
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(
+            Icons.add
+          ),
+          onPressed: () {
+            print('hi There');
+          },
+        ),
+      ),
+    );
+   }
+      
+ }
