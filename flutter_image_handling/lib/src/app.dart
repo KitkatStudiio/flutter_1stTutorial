405584179d6f9960f6ff213f7ff2648dfd708f20
Pranav Kumar Verma <39586967+pranavtechnossus@users.noreply.github.com>
2019-05-19 11:39:45 +0530

    completion of first app
    Completed App for adding image in screen with scroll on click we are adding new image calling api for adding new image

--- flutter_image_handling/lib/src/app.dart
+++ flutter_image_handling/lib/src/app.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' show get;
 import 'models/image_models.dart';
 import 'dart:convert';
+import 'widgets/images_list.dart';
 
 class App extends StatefulWidget{
   
@@ -16,19 +17,25 @@ class App extends StatefulWidget{
  class AppState extends State<App> {
 
    int counter = 0;
-   
+   List<ImageModel> images = [];
+
    void fetchImage()  async {
      counter++;
     var response = await get('https://jsonplaceholder.typicode.com/photos/$counter');
     var imageModel = new ImageModel.fromJson(json.decode(response.body));
     print(imageModel.url);
+    
+    setState(() {
+      images.add(imageModel);
+    });
+    
    }
 
    //Must define a 'build' method that returns the widgets that *this* widget will show
    Widget build(context) {
    return MaterialApp(
       home: Scaffold(
-        body: Text('$counter'),
+        body: ImageList(images),
         appBar: AppBar(title: Text('Lets see some Images')
         ),
         floatingActionButton: FloatingActionButton(
